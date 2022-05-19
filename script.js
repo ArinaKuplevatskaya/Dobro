370da96bd76f7db4109ba273c905db1a8d88ab50
ArinaKuplevatskaya <105887430+ArinaKuplevatskaya@users.noreply.github.com>
2022-05-20 00:16:51 +0300

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger')
+const aside = document.querySelector('aside ul')
+const links = document.querySelectorAll('.menu li a, aside ul li a')
+
+links.forEach(el => {
+   el.onclick = () => {
+      document.body.classList.remove('noScroll')
+      aside.classList.remove('active')
+   }
+})
+
+burger.onclick = () => {
+   document.body.classList.add('noScroll')
+   aside.classList.toggle('active')
+}
